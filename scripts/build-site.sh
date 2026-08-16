52eb74dae565843116a7fed4a59c460f69061a91
Maksim Dementev <orofarne@gmail.com>
2026-08-17 01:25:33 +0400

    Fix scripts/build-site.sh

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -6,6 +6,6 @@
 # Pages' build containers run on.
 set -euo pipefail
 
-curl -fsSL -o /tmp/meshfox.tar.gz "https://github.com/orofarne/meshfox/releases/latest/download/meshfox-linux-x86_64.tar.gz"
+curl -fsSL -o /tmp/meshfox.tar.gz "https://github.com/orofarne/meshfox/releases/latest/download/meshfox-x86_64-unknown-linux-gnu.tar.gz"
 tar xzf /tmp/meshfox.tar.gz -C /tmp
-/tmp/meshfox-linux-x86_64/meshfox static README.md --template ./site-template --out ./site-dist --force
+/tmp/x86_64-unknown-linux-gnu/meshfox static README.md --template ./site-template --out ./site-dist --force
